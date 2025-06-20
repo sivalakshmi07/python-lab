88fd177e6a2d8f79e82a3fef05ea711e962ac527
Sivalakshmi <sivalakshmips37@gmail.com>
2025-06-20 15:22:29 +0530

    Add files via upload

---
+++ printtotalof5nums.py
@@ -0,0 +1,7 @@
+num=int(input("enter the 1st number:"))
+num1=int(input("enter the 2nd number:"))
+num2=int(input("enter the 3rd number:"))
+num3=int(input("enter the 4th number:"))
+num4=int(input("enter the 5th number:"))
+sum=num+num1+num2+num3+num4
+print("Sum=",sum)
