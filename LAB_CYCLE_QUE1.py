79c0e0badb9198f8a7f2e703c522529d640e1c84
Sivalakshmi <sivalakshmips37@gmail.com>
2025-07-10 14:23:41 +0530

    Add files via upload

---
+++ LAB_CYCLE_QUE1.py
@@ -0,0 +1,5 @@
+#Write a program to accept your name and year of birth and display your current age.
+nam=input("Enter your name:- ")
+year=int(input("Enter you year of birth:- "))
+age=2025-year
+print("HELLO ",nam,",YOUR AGE IS ",age)
